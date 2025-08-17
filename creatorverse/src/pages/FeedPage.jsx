d01dec385c051e10084f0f1cd5e3e94bf7eefa5a
justin-t-5 <justintamayd12@gmail.com>
2025-08-17 01:53:03 -0400

    Initial commit

---
+++ creatorverse/src/pages/FeedPage.jsx
@@ -0,0 +1,7 @@
+function FeedPage(){
+    return(
+        <><p>Feed Page</p></>
+    )
+}
+
+export default FeedPage;
---
+++ creatorverse/vite.config.js
@@ -0,0 +1,13 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+    },
+  },
+});
